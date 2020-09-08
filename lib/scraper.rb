71ed456b67e94277a3a71acb8f63c6315dc689cb
kathinka martinsen <kathinkamartinsen@gmail.com>
2020-09-08 14:21:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,23 +1,23 @@
-require 'nokogiri'
+#require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url="https://learn-co-curriculum.github.io/student-scraper-test-page/")
-
+  def self.scrape_index_page
+    index_url ="https://learn-co-curriculum.github.io/student-scraper-test-page/"
     #les siden med nokogiri
-    html = open(index_url)
-    @doc = Nokogiri::HTML(html)
+  #  site = open(index_url)
+    site = Nokogiri::HTML(open(index_url))
     #hent alle studentene
     #lag loop på antall student card
-    @doc.css(".student-card")
+    site.css(".student-card")
     #student navn, lokasjon og url
     #hent ut denne info og lagre i array
     #lag variabel/keys for lagring av info
-    puts @doc.css(".student-name")
-    puts @doc.css(".student-location")
-    puts @doc.css(".student-card a")
+  name = site.css(".student-name")
+  location = site.css(".student-location")
+  profile_url = site.css(".student-card a")
 
     #opprett array med hash
     # :name, :location, :profile_url
